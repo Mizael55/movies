fe27c72c285e8033284581fa89f389af5c7f0103
Mizael55 <misaellsoler@gmail.com>
2023-01-23 17:36:55 -0400

    sliver done

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies/widget/casting_card.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -8,18 +9,25 @@ class DetailsScreen extends StatelessWidget {
     //TODO: Cambiar luego por una instancia de la clase Movie
     final String movie =
         ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
-    return const Scaffold(
+    return Scaffold(
         body: CustomScrollView(
       slivers: [
         _CustomAppBar(),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            _PosterAndTitle(),
+            _OverView(),
+            _OverView(),
+            _OverView(),
+            CastingCards(),
+          ]),
+        )
       ],
     ));
   }
 }
 
 class _CustomAppBar extends StatelessWidget {
-  const _CustomAppBar({super.key});
-
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
@@ -33,9 +41,9 @@ class _CustomAppBar extends StatelessWidget {
           title: Container(
             width: double.infinity,
             color: Colors.black12,
+            padding: const EdgeInsets.only(bottom: 10),
             alignment: Alignment.bottomCenter,
-            child:
-                const Text('movie.title', style: const TextStyle(fontSize: 16)),
+            child: const Text('movie.title', style: TextStyle(fontSize: 16)),
           ),
           background: const FadeInImage(
             placeholder: AssetImage('assets/loading.gif'),
@@ -43,5 +51,68 @@ class _CustomAppBar extends StatelessWidget {
             fit: BoxFit.cover,
           )),
     );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originaTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  const SizedBox(width: 5),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _OverView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        child: Text(
+            'Aliquip ipsum consectetur tempor amet eiusmod irure qui aute. Et ipsum irure exercitation proident qui qui incididunt reprehenderit id. Ad in in Lorem consectetur in reprehenderit elit adipisicing qui labore. Lorem proident sunt nisi elit sda dfs sdfsd cupidatat qui ea.',
+            textAlign: TextAlign.justify,
+            style: Theme.of(context).textTheme.subtitle1));
   }
 }
--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -1,2 +1,4 @@
+export 'package:movies/widget/casting_card.dart';
+
 export 'package:movies/widget/movie_slider.dart';
-export 'package:movies/widget/crad_swiper.dart';
+export 'package:movies/widget/card_swiper.dart';
